1e07170a233efca302128af05bb024965a32a13d
Rohit Ingole <ingolerohit2017@gmail.com>
2023-05-09 11:54:51 +0530

    changes done bro

---
+++ index.js
@@ -0,0 +1,32 @@
+//FUNCTION CALLING
+function ammi_jaan(){
+   var a = 5;
+   var b =6;
+   var sum = a+b;
+   console.log("beta sum is",sum);
+}
+ammi_jaan();
+
+function abbu_jaan(){
+   var a = 5;
+   var b =6;
+   var diff = a-b;
+   console.log("beta diffrence is",diff);
+}
+abbu_jaan();
+
+function pappu_jaan(){
+   var a = 5;
+   var b =6;
+   var mult = a*b;
+   console.log("beta multiplication is",mult);
+}
+pappu_jaan();
+
+function nanu_jaan(){
+   var a = 5;
+   var b =6;
+   var divi = a/b;
+   console.log("beta division is",divi);
+}
+nanu_jaan();
